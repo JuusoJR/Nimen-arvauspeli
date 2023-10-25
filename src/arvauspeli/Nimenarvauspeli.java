ce06d2505cb0a4468985afc951b2974d930eaf92
JuusoJR <juuso.rantonen@gmail.com>
2023-10-25 12:36:17 +0300

    Initial commit

---
+++ src/arvauspeli/Nimenarvauspeli.java
@@ -0,0 +1,36 @@
+package arvauspeli;
+
+import java.util.Scanner;
+
+public class Nimenarvauspeli {
+	public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Tervettuloa arvauspeliin!");
+        System.out.println("Arvaa nimeni");
+        // oikea nimi
+        String correctName = "Matti";
+        // käyttäjä input
+        String guess = "";
+
+
+        do {
+            
+            guess = in.nextLine();
+
+            if (guess.equalsIgnoreCase(correctName)) {
+
+                System.out.println("Onneksi olkoon! Arvasit oikein");
+
+            } else if (guess.equals("")) {
+                System.out.println("Puhu kovempaa! En kuule!");
+            }
+             else {
+                System.out.println("Yritä uudestaan!");
+            }
+        } while (!guess.equalsIgnoreCase(correctName));
+
+    }
+
+}
